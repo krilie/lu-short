dbe981419c0e496c212f717f27231004cbe6cf5a
livo <1197829331@qq.com>
2021-02-26 15:56:22 +0800

    dig support

--- go.mod
+++ go.mod
@@ -1,1 +1,10 @@
-module "lu-short"
+module lu-short
+
+go 1.16
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/stretchr/testify v1.6.1
+	github.com/swaggo/swag v1.7.0
+	go.uber.org/dig v1.10.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+// 短域名 =>限速 限次 限个数 .. 限ip(段) 禁id(多个) 禁ip(段)
+
+//go:generate swag init -g ./main.go  # install: go get -u github.com/swaggo/swag/cmd/swag
+//go:generate go test -run Auto -v ./...
+
 func main() {
 	println("h")
 }
