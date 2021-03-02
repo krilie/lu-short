7640cbfe13494164fe15bd13d76e630e3551bf71
livo <1197829331@qq.com>
2021-03-02 15:48:38 +0800

    redirect message

--- component/broker/broker_list.go
+++ component/broker/broker_list.go
@@ -1,1 +1,17 @@
 package broker
+
+import (
+	go_smq "github.com/krilie/go-smq"
+	"lu-short/component/broker/messages"
+	"reflect"
+)
+
+type Brokers struct {
+	MsgRedirect *go_smq.Broker
+}
+
+func NewBrokers() *Brokers {
+	return &Brokers{
+		MsgRedirect: go_smq.NewStartedBroker(reflect.TypeOf(&messages.MsgRedirect{}), 200),
+	}
+}
